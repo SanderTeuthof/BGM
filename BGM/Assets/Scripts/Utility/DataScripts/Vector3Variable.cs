5ac06742905d688c1b753201f9d36712d132d7b4
SanderTeuthof <jan.teuthof@student.howest.be>
2024-09-08 23:06:39 +0200

    Deleted everything

--- BGM/Assets/Scripts/Utility/DataScripts/Vector3Variable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Vector3Variable", menuName = "DataScripts / Vector3 Variable")]
-public class Vector3Variable : ScriptableObject
-{
-    public Vector3 value;
-}
